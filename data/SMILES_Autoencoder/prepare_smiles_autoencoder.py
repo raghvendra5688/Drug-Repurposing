9196c4bbcb65e351c59940c1c66abcec64ff65bf
Dr. Raghvendra Mall <QCRI\rmall@machinelearning-protein.qcri.org>
2020-10-05 14:45:51 +0300

    Prepared SMILES Autoencoder and added README

--- data/SMILES_Autoencoder/prepare_smiles_autoencoder.py
+++ data/SMILES_Autoencoder/prepare_smiles_autoencoder.py
@@ -4,7 +4,7 @@ import pandas as pd
 
 org_df = pd.read_csv("all_smiles_revised.csv",header=None)
 src = org_df.iloc[:,0].tolist()
-out_list = [[x,x] for x in src]
+out_list = [ [x,x] for x in src if len(x)>=10 and len(x)<=128 ]
 
 out_df = pd.DataFrame(out_list)
 out_df.columns = ['src','tgt']
