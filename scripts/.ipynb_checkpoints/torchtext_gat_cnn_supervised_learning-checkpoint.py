03987c3d1470cceb9532a7e3b24da95ced107d14
Raghvendra Mall <rmall@qf.org.qa>
2020-10-17 10:41:04 +0300

    Added GAT CNN results

--- scripts/.ipynb_checkpoints/torchtext_gat_cnn_supervised_learning-checkpoint.py
+++ scripts/.ipynb_checkpoints/torchtext_gat_cnn_supervised_learning-checkpoint.py
@@ -24,6 +24,7 @@ from torch_geometric.nn import global_add_pool, global_mean_pool
 import matplotlib.pyplot as plt
 import pickle
 
+#Convert SMILES to graph representation
 def smile_to_graph(smile):
     mol = Chem.MolFromSmiles(smile)
     if(mol is None):
@@ -48,18 +49,18 @@ def smile_to_graph(smile):
         return c_size, features, edge_index
 
 
+# +
+#Get compound features
 def one_of_k_encoding_unk(x, allowable_set):
     """Maps inputs not in the allowable set to the last element."""
     if x not in allowable_set:
         x = allowable_set[-1]
     return list(map(lambda s: x == s, allowable_set))
 
-
 def one_of_k_encoding(x, allowable_set):
     if x not in allowable_set:
         raise Exception("input {0} not in allowable set{1}:".format(x, allowable_set))
     return list(map(lambda s: x == s, allowable_set))
-
 
 def atom_features(atom):
     return np.array(one_of_k_encoding_unk(atom.GetSymbol(),['C', 'N', 'O', 'S', 'F', 'Si', 'P', 'Cl', 'Br', 'Mg', 'Na','Ca', 'Fe', 'As', 'Al', 'I', 'B', 'V', 'K', 'Tl', 'Yb','Sb', 'Sn', 'Ag', 'Pd', 'Co', 'Se', 'Ti', 'Zn', 'H','Li', 'Ge', 'Cu', 'Au', 'Ni', 'Cd', 'In', 'Mn', 'Zr','Cr', 'Pt', 'Hg', 'Pb', 'Unknown']) +
@@ -69,6 +70,7 @@ def atom_features(atom):
                     [atom.GetIsAromatic()])
 
 
+# -
 
 class Net(torch.nn.Module):
     def __init__(self):
@@ -80,26 +82,25 @@ class Net(torch.nn.Module):
         self.conv1 = GATConv(78, 78, heads=2, dropout=0.1)
         self.conv2 = GATConv(78*2, 78*3, dropout=0.1)
         self.conv3 = GATConv(78*3, 78 * 4, dropout=0.1)
-        self.fc_g1 = torch.nn.Linear(78*4, 1024)
-        self.bn2 = BatchNorm1d(1024)
-        self.fc_g2 = Linear(1024, 128)
+        self.fc_g1 = torch.nn.Linear(78*4, 256)
+        self.bn2 = BatchNorm1d(256)
+        self.fc_g2 = Linear(256, 64)
        
         
         ## Protein Sequences
-        n_filters = 512
+        n_filters = 128
         self.embedding_xt = nn.Embedding(21 + 1, 128)
         self.conv_xt_1 = nn.Conv1d(in_channels=2000, out_channels=n_filters, kernel_size=3)
-        self.conv_xt_2 = nn.Conv1d(in_channels= 512, out_channels= 128, kernel_size=5)
+        self.conv_xt_2 = nn.Conv1d(in_channels= 128, out_channels= 128, kernel_size=5)
         self.conv_xt_3 = nn.Conv1d(in_channels=128, out_channels=32, kernel_size=8) 
-        #self.conv_xt_4 = nn.Conv1d(in_channels=256, out_channels=512, kernel_size=3)
         self.fc1_xt1 = nn.Linear(32*11, 256)
         self.bn3 = BatchNorm1d(256)
-        self.fc1_xt2 = nn.Linear(256,128)
+        self.fc1_xt2 = nn.Linear(256,64)
 
 
-        self.fc12 = nn.Linear(2*128, 384)
-        self.fc22 = nn.Linear(384, 128)
-        self.out3 = nn.Linear(128, 1)
+        self.fc12 = nn.Linear(2*64, 128)
+        self.fc22 = nn.Linear(128, 64)
+        self.out3 = nn.Linear(64, 1)
         
 
     def forward(self, data):
@@ -122,7 +123,7 @@ class Net(torch.nn.Module):
 
         # flatten
         x = F.relu(self.fc_g1(x))
-        x = F.dropout(x, p=0.25)
+        x = F.dropout(x, p=0.1)
         x = F.relu(self.fc_g2(x))
         
         
@@ -131,30 +132,28 @@ class Net(torch.nn.Module):
         embedded_xt = self.embedding_xt(target)
         conv_xt = F.relu(F.max_pool1d(self.conv_xt_1(embedded_xt),2))
         conv_xt = F.relu(F.max_pool1d(self.conv_xt_2(conv_xt),2))
-        #conv_xt = self.conv_xt_1(embedded_xt)
-        #conv_xt = self.conv_xt_2(conv_xt)
         conv_xt = F.relu(F.max_pool1d(self.conv_xt_3(conv_xt), 2))
         #print("Shape of Conv layer: ", conv_xt.shape)
         #xt = flat(conv_xt)
         xt = conv_xt.view(-1, 32*11)
         #print("Flatten XT shape: ", xt.shape)
         xt = F.relu(self.fc1_xt1(xt))
-        xt = F.dropout(xt, p=0.25)
+        xt = F.dropout(xt, p=0.1)
         xt = F.relu(self.fc1_xt2(xt))
-        xt = F.dropout(xt, p=0.25)
+        xt = F.dropout(xt, p=0.1)
         
         xc = torch.cat((x, xt), 1)
-        #print(xc.shape)
-        #print(len(xc[0]))
         # add some dense layers
         xc = F.relu(self.fc12(xc))
         #xc = F.relu(xc)
-        xc = F.dropout(xc, p=0.2)
+        xc = F.dropout(xc, p=0.1)
         xc = F.relu(self.fc22(xc))
-        xc = F.dropout(xc, p=0.2)
+        xc = F.dropout(xc, p=0.1)
         out = self.out3(xc)
         return out
 
+# +
+#Calculate loss function
 loss_fn = nn.MSELoss()
 best_mse = 1000
 calculated_mse = 1000
@@ -164,9 +163,15 @@ def mse(y,f):
     return mse
 
 
+# -
+
 # ################################## Test Mode #####################################
 
-df = pd.read_csv("Test_Compound_Viral_interactions_for_Supervised_Learning.csv")
+#Option 0 our test file, option 1 SARS-COV-2 tes fiel
+if (option==0):
+    df = pd.read_csv("../data/Test_Compound_Viral_interactions_for_Supervised_Learning.csv")
+else:
+    df = pd.read_csv("../data/sars_cov")
 protein_seqs = df['Sequence'].values.tolist()
 seq_voc_dic = "ACDEFGHIKLMNPQRSTVWXY"
 seq_dict = {voc:idx for idx,voc in enumerate(seq_voc_dic)}
@@ -223,13 +228,7 @@ for smile in smiles:
         smile_graph[smile] = g
 
 
-smile_file = 'smile_graph_2.pkl'
-with open(smile_file, "wb") as f:
-    pickle.dump(smile_graph, f)
-
-smile_file = 'smile_graph_2.pkl'
-with open(smile_file, "rb") as f:
-    smile_graphs = pickle.load(f)
+smile_graphs = smile_graph
 
 data_features = []
 data_edges = []
@@ -266,16 +265,13 @@ calculated_mse = 1000
 def mse(y,f):
     mse = ((y - f)**2).mean(axis=0)
     return mse
-
-
-    
     #device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
 
 device = torch.device('cpu')
 model = Net().to(device)
 optimizer = torch.optim.Adam(model.parameters(), lr=0.0005)
-model.load_state_dict(torch.load('GAT_CNN_2000_3_pooling_checkpoint.pt'))#['state_dict'])
+model.load_state_dict(torch.load('../models/gat_cnn_models/GAT_CNN_2000_3_pooling_checkpoint.pt',map_location=device))#['state_dict'])
 
 model.eval()
 total_preds = torch.Tensor()
--- scripts/torchtext_gat_cnn_supervised_learning.py
+++ scripts/torchtext_gat_cnn_supervised_learning.py
@@ -24,6 +24,7 @@ from torch_geometric.nn import global_add_pool, global_mean_pool
 import matplotlib.pyplot as plt
 import pickle
 
+#Convert SMILES to graph representation
 def smile_to_graph(smile):
     mol = Chem.MolFromSmiles(smile)
     if(mol is None):
@@ -48,18 +49,18 @@ def smile_to_graph(smile):
         return c_size, features, edge_index
 
 
+# +
+#Get compound features
 def one_of_k_encoding_unk(x, allowable_set):
     """Maps inputs not in the allowable set to the last element."""
     if x not in allowable_set:
         x = allowable_set[-1]
     return list(map(lambda s: x == s, allowable_set))
 
-
 def one_of_k_encoding(x, allowable_set):
     if x not in allowable_set:
         raise Exception("input {0} not in allowable set{1}:".format(x, allowable_set))
     return list(map(lambda s: x == s, allowable_set))
-
 
 def atom_features(atom):
     return np.array(one_of_k_encoding_unk(atom.GetSymbol(),['C', 'N', 'O', 'S', 'F', 'Si', 'P', 'Cl', 'Br', 'Mg', 'Na','Ca', 'Fe', 'As', 'Al', 'I', 'B', 'V', 'K', 'Tl', 'Yb','Sb', 'Sn', 'Ag', 'Pd', 'Co', 'Se', 'Ti', 'Zn', 'H','Li', 'Ge', 'Cu', 'Au', 'Ni', 'Cd', 'In', 'Mn', 'Zr','Cr', 'Pt', 'Hg', 'Pb', 'Unknown']) +
@@ -68,6 +69,8 @@ def atom_features(atom):
                     one_of_k_encoding_unk(atom.GetImplicitValence(), [0, 1, 2, 3, 4, 5, 6,7,8,9,10]) +
                     [atom.GetIsAromatic()])
 
+
+# -
 
 class Net(torch.nn.Module):
     def __init__(self):
@@ -79,26 +82,25 @@ class Net(torch.nn.Module):
         self.conv1 = GATConv(78, 78, heads=2, dropout=0.1)
         self.conv2 = GATConv(78*2, 78*3, dropout=0.1)
         self.conv3 = GATConv(78*3, 78 * 4, dropout=0.1)
-        self.fc_g1 = torch.nn.Linear(78*4, 1024)
-        self.bn2 = BatchNorm1d(1024)
-        self.fc_g2 = Linear(1024, 128)
+        self.fc_g1 = torch.nn.Linear(78*4, 256)
+        self.bn2 = BatchNorm1d(256)
+        self.fc_g2 = Linear(256, 64)
        
         
         ## Protein Sequences
-        n_filters = 512
+        n_filters = 128
         self.embedding_xt = nn.Embedding(21 + 1, 128)
         self.conv_xt_1 = nn.Conv1d(in_channels=2000, out_channels=n_filters, kernel_size=3)
-        self.conv_xt_2 = nn.Conv1d(in_channels= 512, out_channels= 128, kernel_size=5)
+        self.conv_xt_2 = nn.Conv1d(in_channels= 128, out_channels= 128, kernel_size=5)
         self.conv_xt_3 = nn.Conv1d(in_channels=128, out_channels=32, kernel_size=8) 
-        #self.conv_xt_4 = nn.Conv1d(in_channels=256, out_channels=512, kernel_size=3)
         self.fc1_xt1 = nn.Linear(32*11, 256)
         self.bn3 = BatchNorm1d(256)
-        self.fc1_xt2 = nn.Linear(256,128)
+        self.fc1_xt2 = nn.Linear(256,64)
 
 
-        self.fc12 = nn.Linear(2*128, 384)
-        self.fc22 = nn.Linear(384, 128)
-        self.out3 = nn.Linear(128, 1)
+        self.fc12 = nn.Linear(2*64, 128)
+        self.fc22 = nn.Linear(128, 64)
+        self.out3 = nn.Linear(64, 1)
         
 
     def forward(self, data):
@@ -121,7 +123,7 @@ class Net(torch.nn.Module):
 
         # flatten
         x = F.relu(self.fc_g1(x))
-        x = F.dropout(x, p=0.25)
+        x = F.dropout(x, p=0.1)
         x = F.relu(self.fc_g2(x))
         
         
@@ -130,30 +132,28 @@ class Net(torch.nn.Module):
         embedded_xt = self.embedding_xt(target)
         conv_xt = F.relu(F.max_pool1d(self.conv_xt_1(embedded_xt),2))
         conv_xt = F.relu(F.max_pool1d(self.conv_xt_2(conv_xt),2))
-        #conv_xt = self.conv_xt_1(embedded_xt)
-        #conv_xt = self.conv_xt_2(conv_xt)
         conv_xt = F.relu(F.max_pool1d(self.conv_xt_3(conv_xt), 2))
         #print("Shape of Conv layer: ", conv_xt.shape)
         #xt = flat(conv_xt)
         xt = conv_xt.view(-1, 32*11)
         #print("Flatten XT shape: ", xt.shape)
         xt = F.relu(self.fc1_xt1(xt))
-        xt = F.dropout(xt, p=0.25)
+        xt = F.dropout(xt, p=0.1)
         xt = F.relu(self.fc1_xt2(xt))
-        xt = F.dropout(xt, p=0.25)
+        xt = F.dropout(xt, p=0.1)
         
         xc = torch.cat((x, xt), 1)
-        #print(xc.shape)
-        #print(len(xc[0]))
         # add some dense layers
         xc = F.relu(self.fc12(xc))
         #xc = F.relu(xc)
-        xc = F.dropout(xc, p=0.2)
+        xc = F.dropout(xc, p=0.1)
         xc = F.relu(self.fc22(xc))
-        xc = F.dropout(xc, p=0.2)
+        xc = F.dropout(xc, p=0.1)
         out = self.out3(xc)
         return out
 
+# +
+#Calculate loss function
 loss_fn = nn.MSELoss()
 best_mse = 1000
 calculated_mse = 1000
@@ -163,8 +163,11 @@ def mse(y,f):
     return mse
 
 
+# -
+
 # ################################## Test Mode #####################################
 
+#Option 0
 df = pd.read_csv("../data/Test_Compound_Viral_interactions_for_Supervised_Learning.csv")
 protein_seqs = df['Sequence'].values.tolist()
 seq_voc_dic = "ACDEFGHIKLMNPQRSTVWXY"
